99354c6bcf57450ac70312fe9e938dc0cb783d9e
Justin Ellison <justin.ellison@buckle.com>
2012-01-31 18:50:49 -0600

    ream -> realm

--- manifests/auth/digest/file/user.pp
+++ manifests/auth/digest/file/user.pp
@@ -14,8 +14,8 @@ define apache::auth::digest::file::user (
   if ! defined(Apache::Module["authn_file"]) {
     apache::module {"authn_file": }
   }
-  if ! defined(Apache::Module["autn_digest"]) {
-    apache::module {"authn_file": }
+  if ! defined(Apache::Module["authn_digest"]) {
+    apache::module {"authn_digest": }
   }
 
   if $authUserFile {
