d940645804b9f461387552937e695e15f9b4af73
Justin Ellison <justin.ellison@buckle.com>
2012-03-02 14:58:42 -0600

    fix for csrfile dependency

--- manifests/vhost-ssl.pp
+++ manifests/vhost-ssl.pp
@@ -292,16 +292,14 @@ define apache::vhost-ssl (
     file { "public CSR file for $name":
       ensure  => $publish_csr ? {
         false   => "absent",
-        default => "present",
+        default => "link",
       },
       path    => $publish_csr ? {
         true    => "${apache::params::root}/${name}/htdocs/${name}.csr",
         false   => "${apache::params::root}/${name}/htdocs/${name}.csr",
         default => $publish_csr,
       },
-      source  => "file://$csrfile",
-      mode    => 640,
-      seltype => "httpd_sys_content_t",
+      target  => $csrfile,
       require => Exec["generate-ssl-cert-$name"],
     }
 
